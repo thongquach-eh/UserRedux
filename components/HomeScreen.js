75469cc68d37026ca9c921faa3822f40cee55f7c
Thong Quach <thong.quach@employmenthero.com>
2021-04-13 16:47:35 +0700

    move functions and styles outside of function component

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-function HomeScreen({ navigation }) {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    welcomeText: {
-      fontSize: 50,
-      textAlign: 'center'
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  welcomeText: {
+    fontSize: 50,
+    textAlign: 'center'
+  }
+});
 
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Welcome to Mobile Training</Text>
--- components/UserListScreen.js
+++ components/UserListScreen.js
@@ -2,65 +2,65 @@ import * as React from 'react';
 import { StyleSheet, Text, FlatList, Image, TouchableHighlight } from 'react-native';
 import userData from '../data/users.json';
 
-function UserListScreen({ navigation }) {
-  const styles = StyleSheet.create({
-    list: {
-    },
-    item : {
-      backgroundColor: 'lightblue',
-      padding: 15,
-      marginVertical: 8,
-      marginHorizontal: 16,
-    },
-    name: {
-      fontSize: 20,
-    },
-    avatar: {
-      width: 72,
-      height: 72,
-    }
-  });
-  
-  const renderName = (name) => (
-    <Text style={styles.name}>
-      {name.title}. {name.first} {name.last}
-    </Text>
+const styles = StyleSheet.create({
+  list: {
+  },
+  item : {
+    backgroundColor: 'lightblue',
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  name: {
+    fontSize: 20,
+  },
+  avatar: {
+    width: 72,
+    height: 72,
+  }
+});
+
+const renderName = (name) => (
+  <Text style={styles.name}>
+    {name.title}. {name.first} {name.last}
+  </Text>
+);
+
+const renderLocation = (loc) => (
+  <Text>
+    {loc.street.number} {loc.street.name}, {loc.city} {loc.state}, {loc.postcode}, {loc.country}
+  </Text>
+)
+
+const renderDob = (dob) => (
+  <Text>
+    {(new Date(dob.date)).toLocaleDateString('default', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    })}
+  </Text>
+)
+
+const renderItem = ({item : user}) => {
+  return (
+    <TouchableHighlight style={styles.item}>
+      <>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: user.picture.medium,
+          }}
+        />
+        {renderName(user.name)}
+        {renderLocation(user.location)}
+        {renderDob(user.dob)}
+      </>
+    </TouchableHighlight>
   );
+};
 
-  const renderLocation = (loc) => (
-    <Text>
-      {loc.street.number} {loc.street.name}, {loc.city} {loc.state}, {loc.postcode}, {loc.country}
-    </Text>
-  )
-
-  const renderDob = (dob) => (
-    <Text>
-      {(new Date(dob.date)).toLocaleDateString('default', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      })}
-    </Text>
-  )
-
-  const renderItem = ({item : user}) => {
-    return (
-      <TouchableHighlight style={styles.item}>
-        <>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri: user.picture.medium,
-            }}
-          />
-          {renderName(user.name)}
-          {renderLocation(user.location)}
-          {renderDob(user.dob)}
-        </>
-      </TouchableHighlight>
-    );
-  };
-
+function UserListScreen({ navigation }) {
   return (
     <FlatList style={styles.list}
       data={userData}
