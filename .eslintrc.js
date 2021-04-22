c260c9ca9bd9d2729d8568036ed75f9cbf004ab7
Kien Tran <kien.tran@employmenthero.com>
2021-04-22 23:50:32 +0700

    Revert "reseet"
    This reverts commit ea5e2a3d204bfa235b39241a28e2eb9a5967346f.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:react-hooks/recommended'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
 };
