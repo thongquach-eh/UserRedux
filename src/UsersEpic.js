d522d8737f99522209ff4ba3ac5942706472c1a9
Thong Quach <thong.quach@employmenthero.com>
2021-05-04 17:24:34 +0700

    retry 3 times when network fails

--- src/UsersEpic.js
+++ src/UsersEpic.js
@@ -4,8 +4,9 @@ import {ACTIONS} from 'redux-api-call';
 import {map, filter} from 'rxjs/operators';
 import {addUsers} from './UsersAction';
 import {combineEpics} from 'redux-observable';
+import {fetchUsersAC} from './state.js';
 
-const fetchUsersEpic = action$ =>
+const fetchUsersCompleteEpic = action$ =>
   action$.pipe(
     ofType(ACTIONS.COMPLETE),
     filter(action => action.payload.name === 'FETCH_USERS'),
@@ -15,6 +16,28 @@ const fetchUsersEpic = action$ =>
     }),
   );
 
-const usersEpic = combineEpics(fetchUsersEpic);
+let fetchUsersNetworkFails: number = 0;
+const fetchUsersNetworkFailureEpic = action$ =>
+  action$.pipe(
+    ofType(ACTIONS.FAILURE),
+    filter(
+      ({payload}) =>
+        payload.name === 'FETCH_USERS' &&
+        payload.json.message === 'Network request failed',
+    ),
+    map(res => {
+      if (++fetchUsersNetworkFails < 3) {
+        return fetchUsersAC();
+      } else {
+        fetchUsersNetworkFails = 0;
+        return {type: 'NO_ACTION'};
+      }
+    }),
+  );
+
+const usersEpic = combineEpics(
+  fetchUsersCompleteEpic,
+  fetchUsersNetworkFailureEpic,
+);
 
 export default usersEpic;
